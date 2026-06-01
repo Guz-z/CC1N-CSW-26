1b11aac20095d94c1028e3632033dc2ccfc303e8
Gustavo <silvagomesgustavo13@gmail.com>
2026-06-01 20:38:56 -0300

    Add files via upload

---
+++ tela de login/code.js
@@ -0,0 +1,10 @@
+function logar(){
+    let login = document.getElementById('login').value;
+    let senha = document.getElementById('pass').value;
+
+    if(login === "tutor" && senha === "123456")
+        window.open("tutor.html")
+    }
+    else if {login === "ong" && senha === "ong$-135"
+
+    }
